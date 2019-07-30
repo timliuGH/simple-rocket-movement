7cfcb25abdcc037b9c93b58993e4bea65b0f7117
timliuGH <timsgithub@gmail.com>
2019-07-30 09:32:46 -0700

    Add background color

--- rocket_main.py
+++ rocket_main.py
@@ -3,17 +3,25 @@ import sys
 import pygame
 
 def run_game():
-    # Initialize game and create a screen object.
+    # Initialize game. 
     pygame.init()
+
+    # Create game display.
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Simple Rocket Movement")
 
-    # Start the main loop for the game.
+    # Set game display background color.
+    bg_color = (230, 230, 230)
+
+    # Start the main event loop for the game.
     while True:
         # Listen for keyboard and mouse events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # Redraw the screen.
+        screen.fill(bg_color)
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
