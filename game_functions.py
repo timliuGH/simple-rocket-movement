a9174a6a7404256787a94aba22d2a4a1e109fc2d
timliuGH <timsgithub@gmail.com>
2019-07-30 11:14:45 -0700

    Refactor keydown and keyup events

--- game_functions.py
+++ game_functions.py
@@ -4,21 +4,29 @@ import sys
 
 import pygame
 
+def check_keydown(event, rocket):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        rocket.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        rocket.moving_left = True
+
+def check_keyup(event, rocket):
+    """Respond to key releases."""
+    if event.key == pygame.K_RIGHT:
+        rocket.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        rocket.moving_left = False
+
 def check_events(rocket):
     """Respond to user events (keypresses, mouse clicks, etc)."""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                rocket.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                rocket.moving_left = True
+            check_keydown(event, rocket)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                rocket.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                rocket.moving_left = False
+            check_keyup(event, rocket)
 
 def update_screen(screen, settings, rocket):
     """Redraw and display game screen."""
